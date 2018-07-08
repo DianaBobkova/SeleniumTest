7390a00d63edd871d2f54b83efe2f1f528d752e3
Nikita Vdoshkin <vdoshkinn@ae.com>
2018-07-08 19:17:43 +0400

    Added .gitignore file and file with configuration

--- src/test/java/ru/yandex/diabobkova/FirstTest.java
+++ src/test/java/ru/yandex/diabobkova/FirstTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Properties;
+import java.io.*;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -16,18 +19,25 @@ public class FirstTest {
     private static WebDriver driver;
 
     @BeforeClass
-    public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "/home/user/Work/chromedriver");
+    public static void setup() throws IOException {
+        Properties property = new Properties();
+        property.load(new FileReader("src/main/resources/application.properties"));
+
+        String host = property.getProperty("pathToDriver");
+
+        System.setProperty("webdriver.chrome.driver", host);
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://mail.ukr.net/desktop/login");
     }
+
     @Test
     public void userLogin() {
-        WebElement loginField = driver.findElement(By.id("login"));
+        WebElement loginField = driver.findElement(By.name("Login"));
         loginField.sendKeys("autotestorgua");
-        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
         passwordField.sendKeys("testpass");
         WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
         loginButton.click();
@@ -35,6 +45,7 @@ public class FirstTest {
         String mailUser = profileUser.getText();
         Assert.assertEquals("autotestorgua@ukr.net", mailUser);
     }
+
     @AfterClass
     public static void tearDown() {
         WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
