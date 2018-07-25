1e40e40695e9fc9fda6903efaf86c3f54c34c8bf
Nikita Vdoshkin <vdoshkinn@ae.com>
2018-07-25 10:54:26 +0400

    First test for e-katalog

--- src/test/java/ru/yandex/diabobkova/FirstTest.java
+++ src/test/java/ru/yandex/diabobkova/FirstTest.java
@@ -1,10 +1,10 @@
 package ru.yandex.diabobkova;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +13,8 @@ import java.util.Properties;
 import java.io.*;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 
 public class FirstTest {
@@ -30,28 +32,37 @@ public class FirstTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://mail.ukr.net/desktop/login");
+        driver.get("http://www.e-katalog.ru/");
     }
 
     @Test
-    public void userLogin() {
-        WebElement loginField = driver.findElement(By.name("Login"));
-        loginField.sendKeys("autotestorgua");
-        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
-        passwordField.sendKeys("testpass");
-        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
-        loginButton.click();
-        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
-        String mailUser = profileUser.getText();
-        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+    public void checkOfCoast() {
+        driver.findElement(By.id("ek-search")).sendKeys("Meizu MX5");
+        driver.findElement(By.id("ek-search")).sendKeys(Keys.RETURN);
+        WebElement MeizuMX5 = driver.findElement(By.id("product_608981"));
+        MeizuMX5.sendKeys(Keys.RETURN);
+        WebElement CoastOfMeizuMX5 = driver.findElement(By.id("price_608981"));
+        CoastOfMeizuMX5.click();
+        WebElement maxPrice = driver.findElement(By.cssSelector("span[itemprop='highPrice']"));
+        Integer integerMaxPrice = Integer.valueOf(maxPrice.getText().replaceAll(" ", ""));
+        assertThat(integerMaxPrice).isGreaterThan(14000);
     }
 
+    //    @Test
+//    public void userLogin() {
+//        WebElement loginField = driver.findElement(By.name("Login"));
+//        loginField.sendKeys("autotestorgua");
+//        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+//        passwordField.sendKeys("testpass");
+//        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
+//        loginButton.click();
+//        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
+//        String mailUser = profileUser.getText();
+//        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+//    }
+//
     @AfterClass
     public static void tearDown() {
-        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
-        menuUser.click();
-        WebElement logoutButton = driver.findElement(By.id("login__logout"));
-        logoutButton.click();
         driver.quit();
     }
 }
